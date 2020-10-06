bda17b850e322e06ea3d9938a58228da7ce226f5
Richard Marquez <rmarqu20@asu.edu>
2020-10-05 19:42:29 -0700

    Initial version.

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total; //private int variable
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //method for getting the total
+  public int getTotal () {
+    return 0; 
+  }
+  //method used for adding to total
+  public void add (int value) {
+  }
+  //method used for subtracting from total
+  public void subtract (int value) {
+  }
+  //method used to keep track of transactions
+  public String toString () {
+    return "";
+  }
+  //method used to clear the total
+  public void clear() {
+  }
+}
